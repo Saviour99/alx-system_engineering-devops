34c3be27c9439bdd5f395ea5fa20da8ca37d5938
Saviour Assandoh <asandohs@gmail.com>
2024-05-06 05:26:53 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,35 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <user_id>")
+        sys.exit(1)
+
     user_id = sys.argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com"
     users_url = f"{base_url}/users/{user_id}"
     todos_url = f"{users_url}/todos"
 
-    response = requests.get(users_url).json()
-    emp_name = response.get("name")
+    try:
+        response = requests.get(users_url).json()
+        emp_name = response.get("name")
 
-    todos = requests.get(todos_url).json()
-    count = 0
+        todos = requests.get(todos_url).json()
+        count = 0
 
-    for todo in todos:
-        if todo.get("completed"):
-            count += 1
+        for todo in todos:
+            if todo.get("completed"):
+                count += 1
 
-    total_todos = len(todos)
+        total_todos = len(todos)
 
-    print("Employee {emp_name} is done with tasks"
-          + "({count}/{total_todos}): ")
-    for todo in todos:
-        if todo.get("completed"):
-            print(f"\t {todo.get('title')}")
+        print("Employee {emp_name} is done with tasks"
+              + "({count}/{total_todos}): ")
+        for todo in todos:
+            if todo.get("completed"):
+                print(f"\t {todo.get('title')}")
+
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data: ", e)
+        sys.exit(1)
