3ec87a75fc18931d9955800d592d0d0dbf8c5e35
Saviour Assandoh <asandohs@gmail.com>
2024-05-07 20:16:19 -0700

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,15 +8,16 @@ import requests
 
 
 def top_ten(subreddit):
+    """A function to get the first ten subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
     resp = requests.get(url, allow_redirects=False)
 
     if resp.status_code == 200:
         data = resp.json()
-        posts = data["data"]["children"]
+        posts = data.get("data")["children"]
         for post in posts:
-            print(post["data"]["title"])
+            print(post.get("data")["title"])
 
     else:
         print(None)
