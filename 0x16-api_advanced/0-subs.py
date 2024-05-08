2daae326439fbd4ba8e07c23a8220ee056b85013
Saviour Assandoh <asandohs@gmail.com>
2024-05-07 20:08:16 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """A function to get the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     resp = requests.get(url, allow_redirects=False)
 
     if resp.status_code == 200:
         data = resp.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+        subscribers = data.get("data")
+        return subscribers.get("subscribers")
     else:
         return 0
