bbeac9ce01d56eadf8467484cbda2ccadcb7d783
Saviour Assandoh <asandohs@gmail.com>
2024-05-07 20:44:39 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,16 +4,19 @@ A function that queries the Reddit API and returns the number of subscribers
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
     """A function to get the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': 'linux:0x16.api.advance:v1.0.0 (by /u/Sav_)'
+    }
 
-    resp = requests.get(url, allow_redirects=False)
+    try:
+        resp = requests.get(url, headers=headers, allow_redirects=False)
+        resp.raise_for_status()  # Raise an exception for 4xx or 5xx errors
+        data = resp.json().get("data")
+        return data.get("subscribers")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
 
-    if resp.status_code == 200:
-        data = resp.json()
-        subscribers = data.get("data")
-        return subscribers.get("subscribers")
-    else:
-        return 0
