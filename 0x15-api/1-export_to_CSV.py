472dbc86b6203dde5a496f49c8e2ee89d202e031
Saviour Assandoh <asandohs@gmail.com>
2024-07-30 20:53:43 -0700

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-"""Python script that returns information using a REST API"""
-
+""" Export api to csv"""
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """ANYTHING"""
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
 
-    user_id = int(sys.argv[1])
-
-    base_url = "https://jsonplaceholder.typicode.com"
-    users_url = f"{base_url}/users/{user_id}"
-    todos_url = f"{users_url}/todos"
-
-    try:
-        response = requests.get(users_url).json()
-        emp_name = response.get('name')
-
-        todos = requests.get(todos_url).json()
-
-        csv_filename = f"{user_id}.csv"
-
-        with open(csv_filename, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["USER_ID", "USERNAME",
-                             "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            for todo in todos:
-                emp = "'{}'".format(str(emp_name))
-                user = "'{}'".format(str(user_id))
-                completed_status = "'{}'".format(str(todo['completed']))
-                task_title = "'{}'".format(str(todo['title']))
-
-                writer.writerow([user, emp, completed_status, task_title])
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data: ", e)
-        sys.exit(1)
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """Complete"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
