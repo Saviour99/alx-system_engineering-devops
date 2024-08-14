9ded9f50dfc08c8d9f35047e55d265ba034fed9b
Saviour Assandoh <asandohs@gmail.com>
2024-08-13 17:32:53 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """A function that queries the Reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    urlEndPoint = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'custom-user-agent'}
 
     try:
-        resp = requests.get(url, headers=headers, allow_redirects=False)
+        resp = requests.get(urlEndPoint, headers=headers, allow_redirects=False)
 
         if resp.status_code != 200:
             return 0
