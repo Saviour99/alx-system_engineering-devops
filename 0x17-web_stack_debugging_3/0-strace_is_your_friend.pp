a93bed560b5e8c80cb2dded7925e8d912e946fa7
Saviour Assandoh <asandohs@gmail.com>
2024-08-15 21:49:13 -0700

    apache2 debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixing "phpp" to "php" in wp-settings.php file
 
-exec{'fix-wordpress':
+exec{'fix-wordpress extention error':
 	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 	path	=> '/usr/local/bin/:/bin'	
 }
